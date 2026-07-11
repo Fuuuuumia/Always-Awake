b217b503513a16185b85342808ddeb34716c4b0c
ʕ•̫͡•ʕ•̫͡•ʔ <fumi.r35@gmail.com>
2026-07-11 10:18:12 +0900

    fix: modified some val name

--- app/src/main/java/com/fumibase/alwaysawake/AwakeActivity.kt
+++ app/src/main/java/com/fumibase/alwaysawake/AwakeActivity.kt
@@ -44,7 +44,7 @@ class AwakeActivity : ComponentActivity() {
 
     companion object {
         const val EXTRA_CHARGE_ONLY = "extra_charge_only"
-        const val EXTRA_SHOW_SCREEN_SAVER = "extra_show_awake_icon"
+        const val EXTRA_SHOW_SCREEN_SAVER = "extra_show_screen_saver"
         const val EXTRA_FINISH_REASON = "extra_finish_reason"
         const val REASON_UNPLUGGED = "reason_unplugged"
     }
@@ -71,7 +71,7 @@ class AwakeActivity : ComponentActivity() {
 
         setContent {
             BlackScreen(
-                showAwakeIcon = showScreenSaver,
+                showScreenSaver = showScreenSaver,
                 onDoubleTap = { finish() }
             )
         }
@@ -135,7 +135,7 @@ class AwakeActivity : ComponentActivity() {
 
 @Composable
 private fun BlackScreen(
-    showAwakeIcon: Boolean,
+    showScreenSaver: Boolean,
     onDoubleTap: () -> Unit
 ) {
     Box(
@@ -146,7 +146,7 @@ private fun BlackScreen(
                 detectTapGestures(onDoubleTap = { onDoubleTap() })
             }
     ) {
-        if (showAwakeIcon) {
+        if (showScreenSaver) {
             AwakeIndicator()
         }
     }
